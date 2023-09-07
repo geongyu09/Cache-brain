bc33e57cf4fe3c79a075de5b94f7a6848a063952
박건규 <geongyu09@gmail.com>
2023-09-07 20:38:45 +0900

    make user schemas

---
+++ sanity-studio/schemas/learningCard.ts
@@ -0,0 +1,17 @@
+export default {
+  name: 'learningCard',
+  type: 'document',
+  title: 'learningCard',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+    },
+    {
+      name: 'destribute',
+      type: 'string',
+      title: 'Destribute',
+    },
+  ],
+}
