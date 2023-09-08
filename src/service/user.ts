7de26f86633c71fcf3b9e09d8ea89d6e80a9978f
박건규 <geongyu09@gmail.com>
2023-09-08 16:49:34 +0900

    change user api to store user image

--- src/service/user.ts
+++ src/service/user.ts
@@ -1,11 +1,12 @@
 import { User } from "next-auth";
 import { client } from "./sanity";
 
-export async function makeNewUser({ name, email, id }: User) {
+export async function makeNewUser({ name, email, id, image }: User) {
   const newUser = {
     _id: id,
     name,
     username: email?.split("@")[0] || "",
+    image,
     _type: "user",
   };
   try {
