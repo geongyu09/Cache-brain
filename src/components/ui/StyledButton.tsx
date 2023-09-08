3d3bb793b6f6c450c634f27e0964febcaa8637a2
박건규 <geongyu09@gmail.com>
2023-09-08 15:37:06 +0900

    fix: styledbutton padding

--- src/components/ui/StyledButton.tsx
+++ src/components/ui/StyledButton.tsx
@@ -12,7 +12,7 @@ export default function StyledButton({ text, handler }: Props) {
   return (
     <button
       onClick={handler}
-      className="px-4 py-4 rounded-lg bg-indigo-600 ring-1 ring-slate-400 hover:opacity-80 text-slate-200"
+      className="px-4 py-2 rounded-lg bg-indigo-600 ring-1 ring-slate-400 hover:opacity-80 text-slate-200"
     >
       {text}
     </button>
