4f60667ca46189d13cb1470e6ca8a06d4d7aee47
박건규 <geongyu09@gmail.com>
2023-09-09 18:37:11 +0900

    make CardListContent to reusable

--- src/components/CardListSection.tsx
+++ src/components/CardListSection.tsx
@@ -5,9 +5,13 @@ import { Card } from "@/model/card";
 import CardList from "./CardList";
 import CardDetail from "./CardDetail";
 
-export default function CardListContent() {
+type Props = {
+  typeOwn: boolean;
+};
+
+export default function CardListContent({ typeOwn }: Props) {
   const [focus, setFocus] = useState("");
-  const GET_ALL_CARD_URL = "/api/card/all";
+  const GET_ALL_CARD_URL = `/api/card/${typeOwn ? "own" : "all"}`;
   const { data: cards, isLoading, error } = useSWR<Card[]>(GET_ALL_CARD_URL);
   if (!cards) return;
 
