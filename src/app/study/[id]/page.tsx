23fe8c8f48855d75972c306656151b8588e4c876
박건규 <geongyu09@gmail.com>
2023-09-11 17:06:26 +0900

    refec: MainIcon

--- src/app/study/[id]/page.tsx
+++ src/app/study/[id]/page.tsx
@@ -5,5 +5,7 @@ type Props = {
 };
 
 export default function page({ params: { id } }: Props) {
+  //학습 카드를 만들기. => 서버상에서 동작해서 만들어줌
+  //이후에 데이터를 받아옴.
   return <div>study {id}</div>;
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import HeaderForm from "./HeaderForm";
 import Link from "next/link";
 import NewCardButton from "./NewCardButton";
+import MainIcon from "./MainIcon";
 
 export default function Header() {
   return (
     <header className="w-full flex justify-between items-center px-4 py-4 border-b-2">
+      <MainIcon position="header" />
       <HeaderForm />
       <nav className="flex w-full max-w-lg justify-between items-center">
         <Link href={"/"}>
