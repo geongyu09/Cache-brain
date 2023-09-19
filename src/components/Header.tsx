7ded363d3fa82eb54d615b6f95239ba552e750be
박건규 <geongyu09@gmail.com>
2023-09-19 21:08:55 +0900

    fix header link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,16 @@ import Link from "next/link";
 import NewCardButton from "./NewCardButton";
 import MainIcon from "./MainIcon";
 import RouteBackButton from "./RouteBackButton";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/service/auth";
 
 type Props = {
   position: "study" | "detail" | "new";
 };
 
-export default function Header({ position }: Props) {
+export default async function Header({ position }: Props) {
+  const session = await getServerSession(authOptions);
+  const username = session?.user.username;
   return (
     <header className="w-full flex justify-between items-center px-4 py-4 border-b-2">
       {position == "detail" ? <HeaderForm /> : <MainIcon position="header" />}
@@ -17,10 +21,12 @@ export default function Header({ position }: Props) {
         <Link href={"/"}>
           <h3>home</h3>
         </Link>
+        <Link href={`/${username}`}>
+          <h3>cards</h3>
+        </Link>
         <Link href={"/contact"}>
           <h3>contact</h3>
         </Link>
-        <h3>surpport</h3>
         {position !== "new" ? <NewCardButton /> : <RouteBackButton />}
       </nav>
     </header>
