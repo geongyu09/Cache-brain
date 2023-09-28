5b4f1ea615b1b3d6e7ce398d3eec5ca72415ac45
박건규 <geongyu09@gmail.com>
2023-09-28 23:35:29 +0900

    make ui

--- src/components/EditContentsSection.tsx
+++ src/components/EditContentsSection.tsx
@@ -8,5 +8,22 @@ type Props = {
 };
 
 export default function EditContentsSection({ card }: Props) {
-  return <section></section>;
+  return (
+    <section>
+      <ul className="w-full max-h-screen p-5 overflow-auto pb-[70px]">
+        {card.content.map((item) => (
+          <li key={item._key} className="bg-slate-200 mb-5 p-5 rounded-lg">
+            <input
+              type="text"
+              className="w-full mb-2 bg-inherit outline-none"
+              value={item.problem}
+            />
+            <textarea className="w-full bg-inherit outline-none resize-none overflow-auto">
+              {item.answer}
+            </textarea>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
