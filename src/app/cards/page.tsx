20c5862cd0a528c2128b2351cfe38bf1c4dad8bd
박건규 <geongyu09@gmail.com>
2023-10-02 12:25:43 +0900

    change redirect

--- src/app/cards/page.tsx
+++ src/app/cards/page.tsx
@@ -4,15 +4,9 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import React from "react";
 
-type Props = {
-  params: {
-    userId: string;
-  };
-};
-
-export default async function UserDetailPage({ params: { userId } }: Props) {
+export default async function UserDetailPage() {
   const session = await getServerSession(authOptions);
   const user = session?.user;
-  if (!session || user?.username !== userId) redirect("/");
-  return <CardListContent typeOwn={false} username={user.username} />;
+  if (!session) redirect("/");
+  return <CardListContent typeOwn={false} username={user!.username} />;
 }
