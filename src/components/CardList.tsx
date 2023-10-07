0916363c6eb180e1b67b0848b26761ad8ce21c16
박건규 <geongyu09@gmail.com>
2023-10-07 20:07:50 +0900

    make if no card Ui

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import List from "./List";
 import { Card } from "@/model/card";
+import NewCardButton from "./NewCardButton";
+import Link from "next/link";
 
 type Props = {
   cards: Card[];
@@ -10,6 +12,14 @@ type Props = {
 export default function CardList({ cards, modify }: Props) {
   return (
     <ul className="grow w-full h-5/6 mx-5 overflow-y-auto">
+      {cards.length == 0 || cards == undefined ? (
+        <div>
+          no cards.. How about make your own card!{"  "}
+          <Link href={"/new"} className="text-indigo-600">
+            click here!
+          </Link>
+        </div>
+      ) : null}
       {cards.map((card) => (
         <List
           key={card.id}
