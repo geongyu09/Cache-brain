47467e3eab433130c19280b651277db598e1821c
박건규 <geongyu09@gmail.com>
2023-11-03 23:19:36 +0900

    add Editpage input placeholder

--- src/components/EditDetailSection.tsx
+++ src/components/EditDetailSection.tsx
@@ -24,6 +24,7 @@ export default function EditDetailSection({ editCard, setEditCard }: Props) {
   return (
     <section className="flex flex-col w-full h-[100vh] bg-slate-100 px-2 pb-[80px]">
       <input
+        placeholder="Title of Card"
         className="w-full p-2 bg-inherit outline-none text-lg my-3"
         value={editCard.title}
         onChange={(e) => {
@@ -32,6 +33,7 @@ export default function EditDetailSection({ editCard, setEditCard }: Props) {
       />
       <hr />
       <textarea
+        placeholder="Card Description"
         className="w-full outline-none bg-inherit text-lg mt-6 resize-none grow"
         value={editCard.description}
         onChange={(e) => {
