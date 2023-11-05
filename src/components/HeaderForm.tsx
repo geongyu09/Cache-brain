59d574534fb08b3645220e2b9d83b9bbeb969bee
박건규 <geongyu09@gmail.com>
2023-11-05 17:49:24 +0900

    fix form ui

--- src/components/HeaderForm.tsx
+++ src/components/HeaderForm.tsx
@@ -11,7 +11,7 @@ export default function HeaderForm({ style }: Props) {
     <form className={`hidden lg:inline ${style}`}>
       <input
         type="text"
-        className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 outline-none"
+        className="block w-80 rounded-md border-0 py-1.5 px-7 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 outline-none"
         placeholder="Search Card"
         value={keyword}
         onChange={(e) => setKeyword(e.target.value)}
